df7b9c095c1d7ed8f265fc6d3ba88dd314b516e2
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2019-12-13 14:54:18 -0200

    Template dinâmico e classe pai para view

--- alurabank/app/js/views/NegociacoesView.js
+++ alurabank/app/js/views/NegociacoesView.js
@@ -1,11 +1,5 @@
-class NegociacoesView {
-    constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
-    }
-    update() {
-        this._elemento.innerHTML = this.template();
-    }
-    template() {
+class NegociacoesView extends View {
+    template(model) {
         return `
         
         <table class="table table-hover table-bordered">
@@ -19,6 +13,14 @@ class NegociacoesView {
             </thead>
 
             <tbody>
+                ${model.paraArray().map(negociacao => `
+                        <tr>
+                            <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
+                            <td>${negociacao.volume}</td>
+                        </tr>
+                    `).join('')}
             </tbody>
 
             <tfoot>
